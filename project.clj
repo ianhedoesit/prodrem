701272922c676c6e8fdcbf3d4a4439c35b22a9a9
Ian Harris <ianhedoesit@gmail.com>
2015-01-22 11:57:16 +0000

    Bogus commit for testing tentacles.

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject prodrem "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Productivity reminder based on git commits."
+  :url "http://ianhedoesit.com/prodrem"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [tentacles "0.3.0"]
+                 [hiccup "1.0.5"]
+                 [com.draines/postal "1.11.3"]
+                 [overtone/at-at "1.2.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler prodrem.core.handler/app}
   :profiles
--- src/prodrem/core/handler.clj
+++ src/prodrem/core/handler.clj
@@ -1,9 +1,20 @@
 (ns prodrem.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :as response]
+            [tentacles.repos :as repos]))
+
+(defn post-user-route
+  "Expects a username"
+  [request]
+  (let [user (get-in request [:params :user])]
+    ;;(query/insert-contact<! {:user user})
+    ;;(repos)
+    (response/redirect "/")))
 
 (defroutes app-routes
+  (POST "/user" [] post-user-route)
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
